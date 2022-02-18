69e52047452edf7d8169292b6efe5a4c6ebd5b72
JJEsp7 <76466626+JuanE267@users.noreply.github.com>
2022-02-18 15:18:01 -0300

    SIRIWAVE IMPLEMENTATION
    siriwave.js library update and another radio station

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 let play = document.getElementById('play');
 let ESTASESCUCHANDO = document.querySelector('#ESTASESCUCHANDO');
+let wave = document.querySelector("#example");
 
-document.getElementById('play').onclick = function() {
+
+play.onclick = function() {
+
+    audio.play();
     play.classList.toggle('fade');
     setTimeout(() => {
         play.classList.toggle('display')
     }, 600);
     setTimeout(() => {
         ESTASESCUCHANDO.classList.toggle('fade');
+        wave.classList.toggle('fade');
     }, 600);
-}
+};
+
+var classic = new SiriWave({
+    container: wave,
+    width: 800,
+    height: 150,
+    color: '#f8aa28',
+    speed: 0.07,
+});
